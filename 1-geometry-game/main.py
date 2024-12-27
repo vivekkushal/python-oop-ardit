7872d60cab3fd9a1753a454113c3653f79a5ac55
vivekkushal <vivekjss1@gmail.com>
2024-12-27 16:39:59 +0530

    feat: Add class

---
+++ 1-geometry-game/main.py
@@ -0,0 +1,19 @@
+# class Person:
+    
+#     def __init__(self, n, a):
+#         self.name = n
+#         self.age = a
+
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+point1 = Point(3, 4)
+point2 = Point(6, 7)
+
+print(point1)
+print(point2)
