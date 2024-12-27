812cbeedd3082937cc5eff19ed3aecd83868efea
vivekkushal <vivekjss1@gmail.com>
2024-12-27 18:39:41 +0530

    feat: Add distance method

--- 1-geometry-game/main.py
+++ 1-geometry-game/main.py
@@ -11,9 +11,20 @@ class Point:
         self.x = x
         self.y = y
 
+    def falls_in_rectangle(self, lower_left, upper_right):
+        if (self.x > lower_left[0] and self.x < upper_right[0]) \
+            and (self.y > lower_left[1] and self.y < upper_right[1]):
+            return True
+        return False
+    
+    def distance(self, x, y):
+        return ((self.x - x) ** 2 + (self.y - y) ** 2) ** 0.5
+
 
 point1 = Point(3, 4)
 point2 = Point(6, 7)
 
-print(point1)
-print(point2)
+point3 = Point(4, 5)
+
+print(point3.falls_in_rectangle((3, 4),(6, 7)))
+print(point1.distance(0, 0))
