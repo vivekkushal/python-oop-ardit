4ad121c44037050756b62ed7cf40e05765a2db94
vivekkushal <vivekjss1@gmail.com>
2024-12-28 09:51:47 +0530

    feat: Add user input, rectangle area

--- 1-geometry-game/main.py
+++ 1-geometry-game/main.py
@@ -28,13 +28,29 @@ class Rectangle:
         self.lower_left = lower_left
         self.upper_right = upper_right
 
+    def area(self):
+        length = self.upper_right.x - self.lower_left.x
+        breadth = self.upper_right.y - self.lower_left.y
+        return length * breadth
 
 
-point1 = Point(3, 4)
-point_origin = Point(0, 0)
 
-print(point1.distance_from_point(point_origin))
+# point1 = Point(3, 4)
+# point_origin = Point(0, 0)
 
-pointx = Point(6, 7)
-rectanglex = Rectangle(Point(5, 6), Point(7, 9))
-print(pointx.falls_in_rectangle(rectanglex))
+# print(point1.distance_from_point(point_origin))
+
+# pointx = Point(6, 7)
+# rectanglex = Rectangle(Point(5, 6), Point(7, 9))
+# print(pointx.falls_in_rectangle(rectanglex))
+
+from random import randint
+
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)), Point(randint(10, 19), randint(10, 19)))
+print("Rectangle coordinates:", rectangle.lower_left.x, rectangle.lower_left.y, \
+    "and", rectangle.upper_right.x, rectangle.upper_right.y)
+
+x = int(input("Guess 'x' coordinate: "))
+y = int(input("Guess 'y' coordinate: "))
+point = Point(x, y)
+print("Your point was inside the rectangle:", point.falls_in_rectangle(rectangle))
