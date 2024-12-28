78561da60005f87eb4231a944a311abecf023936
vivekkushal <vivekjss1@gmail.com>
2024-12-28 23:52:49 +0530

    test: Rectangle traverse

--- 1-geometry-game-gui/main.py
+++ 1-geometry-game-gui/main.py
@@ -1,6 +1,14 @@
 import turtle
 
+# create a canvas instance
 my_turtle = turtle.Turtle()
+
+my_turtle.forward(100)  # move 100 pixels forward
+my_turtle.left(90)  # turn 90 degrees left
+my_turtle.forward(200)
+my_turtle.left(90)
 my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(200)
 
 turtle.done()
