b84317b1c4bd1039addd0c1b023e4c5592156851
vivekkushal <vivekjss1@gmail.com>
2024-12-28 08:41:36 +0530

    feat: Rectangle class

--- 1-geometry-game/main.py
+++ 1-geometry-game/main.py
@@ -1,5 +1,5 @@
 # class Person:
-    
+
 #     def __init__(self, n, a):
 #         self.name = n
 #         self.age = a
@@ -11,22 +11,30 @@ class Point:
         self.x = x
         self.y = y
 
-    def falls_in_rectangle(self, lower_left, upper_right):
-        if (self.x > lower_left[0] and self.x < upper_right[0]) \
-            and (self.y > lower_left[1] and self.y < upper_right[1]):
+    def falls_in_rectangle(self, rectangle):
+        if (self.x > rectangle.lower_left.x and self.x < rectangle.upper_right.x) \
+            and (self.y > rectangle.lower_left.y and self.y < rectangle.upper_right.y):
             return True
         return False
-    
+
     def distance_from_point(self, point):
         return ((self.x - point.x) ** 2 +
                 (self.y - point.y) ** 2) ** 0.5
 
 
-point1 = Point(3, 4)
-point2 = Point(6, 7)
+class Rectangle:
 
-point3 = Point(4, 5)
+    def __init__(self, lower_left, upper_right):
+        self.lower_left = lower_left
+        self.upper_right = upper_right
+
+
+
+point1 = Point(3, 4)
 point_origin = Point(0, 0)
 
-print(point3.falls_in_rectangle((3, 4),(6, 7)))
 print(point1.distance_from_point(point_origin))
+
+pointx = Point(6, 7)
+rectanglex = Rectangle(Point(5, 6), Point(7, 9))
+print(pointx.falls_in_rectangle(rectanglex))
